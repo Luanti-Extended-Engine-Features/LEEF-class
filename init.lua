52fdebfcdb049a6b603ee87f22cee7ed080f05cb
FatalErr42O <58855799+FatalError42O@users.noreply.github.com>
2024-06-26 21:19:24 -0700

    fixed class namespace not existing

--- init.lua
+++ init.lua
@@ -1,8 +1,8 @@
-local path = minetest.get_modpath("mtul_class")
-dofile(path.."/proxy_table.lua")
-dofile(path.."/new_class.lua")
-
 if not mtul then
     mtul = {}
 end
 mtul.class = {}
+
+local path = minetest.get_modpath("mtul_class")
+dofile(path.."/proxy_table.lua")
+dofile(path.."/new_class.lua")
