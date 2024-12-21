95895eb37702d373cf4f463f66508d51d54d8ba6
FatalErr42O <58855799+FatalError42O@users.noreply.github.com>
2024-12-20 23:03:01 -0800

    removed individual documentation in favor of a master site

--- docs/index.js
+++
@@ -1,8 +0,0 @@
-var docs = [
-{path:"class/new_class.html", type:"class", title:"new_class.new_class", text:"An instantiatable class to inherit for defining new instantiatble classes classes the \"base\" class. To make a new class call new_class:inherit(your_new_class). also note that these classes will not have the type \"table\" but \"class\"."},
-{path:"class/new_class.html#new_class.instance", type:"field", title:"new_class.instance", text:"defines wether the object is an instance, use this in construction to determine what changes to make"},
-{path:"class/new_class.html#new_class.base_class", type:"field", title:"new_class.base_class", text:"only present for instances: the class from which this instance originates"},
-{path:"class/new_class.html#new_class.parent_class", type:"field", title:"new_class.parent_class", text:"the class from which this class was inherited from"},
-{path:"class/new_class.html#leef.class.new_class.inherit", type:"function", title:"leef.class.new_class:inherit", text:"creates a new base class. Calls all constructors in the chain with def.instance=true"},
-{path:"class/new_class.html#leef.class.new_class.new", type:"function", title:"leef.class.new_class:new", text:"creates an instance of the base class. Calls all constructors in the chain with def.instance=true"},
-];
--- generate_docs/luadox/run.bat
+++
@@ -1,4 +0,0 @@
-::literally just so I dont have to open powershell every time. 
-::python 3.8+ required.
-@echo off
-python ./luadox -c ./luadox.conf
