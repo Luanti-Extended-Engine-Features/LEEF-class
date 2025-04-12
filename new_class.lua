054eb7a4429a8e3b0da0f279ab1beb337941a4fa
FatalErr42O <58855799+FatalError42O@users.noreply.github.com>
2025-04-11 17:12:13 -0700

    verify no changes

--- new_class.lua
+++ new_class.lua
@@ -89,6 +89,7 @@ function leef.class.new_class:new_class(def)
 
     return def
 end
+
 ---deprecated. The same as new_class, but has settings differences.
 -- @function inherit
 function leef.class.new_class:inherit(def)
